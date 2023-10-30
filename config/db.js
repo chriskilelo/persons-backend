8da63db88bcd150066d37cccec2520b41f08294c
chriskilelo <chriskilelo@gmail.com>
2023-10-30 08:47:12 +0300

    Create the Mongo DB configuration file db.js

---
+++ config/db.js
@@ -0,0 +1,27 @@
+/**
+ * TO GOD BE ALL THE GLORY
+ * 29-OCT-2023
+ * This module will handle Mongo DB connectivity
+ */
+
+const mongoose = require('mongoose')
+
+const connectDB = async () => {
+    try {
+        // Open Mongoose's default connection to MongoDB
+        const conn = await mongoose.connect(process.env.MONGO_URI)
+        // After successful database connection, log a message to the console
+        console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
+    } catch (error) {
+        // In the event of an error, log it on the console
+        console.log(error)
+        // Terminate the running process synchronously 
+        process.exit(1)
+    }
+}
+
+/**
+ * Export the functions and values in this module to allow 
+ * for them to be accessed and used in other parts of this application
+ */
+module.exports = connectDB
